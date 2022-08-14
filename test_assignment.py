c26d763d1ce6e02d222d1c9a7237d63f3cd81338
Sercan Dikici <sercandikici1@gmail.com>
2022-08-14 15:41:53 +0200

    14.08 commit

--- test_assignment.py
+++ test_assignment.py
@@ -1,27 +1,25 @@
 import unittest
 import import_ipynb #install this if necessary
 from assignment import Database
+from assignment import Visualization
+from unittest import mock
+from bokeh.plotting import figure
+import numpy as np
 
 
-class TestAssignment(unittest.TestCase):
+class TestDatabase(unittest.TestCase):
 
-    def setUp(self): #run the code before every test
-    #we can create the employees here and remove from the ones below
-    # dont forget to add self. , dont use it if no setup
-        print("\n")
-        print("setUp", end=" ")   
+    def setUp(self):
+ 
         self.data1 = Database("train.csv","train") 
         self.data2 = Database("ideal.csv","ideal")
 
     def tearDown(self):#run the code after every test
-        print("tearDown")
-    
-    def test_csvfile(self):
-        print("test_csvname",end=" ")
-           
-        self.assertEqual(self.data1.csvfile, "train.csv")
-        self.assertEqual(self.data2.csvfile, "ideal.csv")
+        pass
 
+    def test_csvfile(self):
+        print("Tests are : test_csvname",end=", ")
+           
         self.data1.csvfile = "john.csv"
         self.data2.csvfile = "jane.csv"
 
@@ -29,13 +27,25 @@ class TestAssignment(unittest.TestCase):
         self.assertEqual(self.data2.csvfile, "jane.csv")  
     
     def test_tablename(self):
-        print("test_tablename",end=" ")
+        print("test_tablename",end=", ")
            
-        self.assertEqual(self.data1.tablename, "train")
-        self.assertEqual(self.data2.tablename, "ideal")
+        self.data1.tablename = "xavi"
+        self.data2.tablename = "iniesta"
 
-        self.data1.tablename = "ronaldo"
-        self.data2.tablename = "messi"
+        self.assertEqual(self.data1.tablename, "xavi")
+        self.assertEqual(self.data2.tablename, "iniesta")  
 
-        self.assertEqual(self.data1.tablename, "ronaldo")
-        self.assertEqual(self.data2.tablename, "messi")  
+
+
+class TestVisual(unittest.TestCase):
+
+    def test_visualization(self):
+        print("test_visualization",end=" ")
+        x = np.arange(1,10,0.1)
+        y = np.arange(11,20,0.1)
+        model = Visualization(x,y,4,"title")
+        plot = model.train()
+        self.assertEqual(str(type(plot)),"<class 'bokeh.plotting.figure.Figure'>")
+
+if __name__=="__main__":
+    unittest.main()
